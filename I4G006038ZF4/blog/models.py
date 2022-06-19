9f405b58dc6a06cf890714eafa34baeaeb596890
aliaformo <aliaformo@gmail.com>
2022-06-19 03:32:44 +0200

    'adding'

---
+++ I4G006038ZF4/blog/models.py
@@ -0,0 +1,21 @@
+from multiprocessing import AuthenticationError
+import string
+from turtle import title
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    
+    created_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now=True)
+        
+    def __str__(self) -> str:
+        return self.title
+    
+    
